8f26b3aa9cec8922b182e67f706ec5020f40083a
christiangoran <christian.goran@protonmail.com>
2024-07-15 13:38:12 +0200

    updated address in api

--- src/api/axiosDefaults.jsx
+++ src/api/axiosDefaults.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL =
+  "https://crypto-tracker-heroku-b5acda38c706.herokuapp.com/";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
