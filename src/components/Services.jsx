0b178a15f5eaaf9bd0ce3b88ca7be282ba6a5c53
christiangoran <christian.goran@protonmail.com>
2024-07-16 09:26:13 +0200

    Update width of image in bentobox

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -23,7 +23,7 @@ const Services = () => {
         <div className="relative">
           {/* Top bento box */}
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5">
               <img
                 className="object-cover w-full h-full md:object-right"
                 src={service1}
